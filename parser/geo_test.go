8d1333bd1c55ce23cbda880a2e465bed0ff0f219
Antoine POPINEAU <antoine.popineau@appscho.com>
2016-02-11 17:11:36 +0100

    Update tests.

--- parser/geo_test.go
+++ parser/geo_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func Test_ParseGeo(t *testing.T) {
-	lat, long, err := ParseGeo("32.745,128.45")
+	lat, long, err := ParseGeo("32.745;128.45")
 
 	assert.Equal(t, nil, err)
 
@@ -16,7 +16,7 @@ func Test_ParseGeo(t *testing.T) {
 }
 
 func Test_ParseGeoError(t *testing.T) {
-	lat, long, err := ParseGeo("hello,128.45")
+	lat, long, err := ParseGeo("hello;128.45")
 
 	assert.NotEqual(t, nil, err)
 
