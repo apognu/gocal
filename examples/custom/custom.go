d910d4fca73c211831557f0a641c71acc18ee38e
Gideon Potok <g.potok4@gmail.com>
2022-05-29 12:20:39 -0400

    change in opinionation about https://icalendar.org/iCalendar-RFC-5545/3-3-4-date.html

--- examples/custom/custom.go
+++ examples/custom/custom.go
@@ -29,6 +29,19 @@ SUMMARY:Second event with custom labels
 X-ROOMID:802-127A
 X-COLOR:#ffffff
 END:VEVENT
+
+BEGIN:VEVENT
+DTSTAMP:20151116T133227Z
+DTSTART;VALUE=DATE:20210915T000000
+DTSTAMP:20151116T133227Z
+DTEND;VALUE=DATE:20210917T000000
+UID:three@gocal
+SUMMARY:Third event with custom Date
+X-ROOMID:802-127A
+X-COLOR:#ffffff
+SEQUENCE:0
+END:VEVENT
+
 END:VCALENDAR
 `
 
@@ -44,6 +57,7 @@ func main() {
 	c.Parse()
 
 	for _, e := range c.Events {
-		fmt.Printf("%s on %s - %s\n", e.Summary, e.CustomAttributes["X-ROOMID"], e.CustomAttributes["X-COLOR"])
+		fmt.Printf("%s on %s - %s from %s To %s\n", e.Summary, e.CustomAttributes["X-ROOMID"], e.CustomAttributes["X-COLOR"],
+			e.Start.Format("2006-01-02"), e.End.Format("2006-01-02"))
 	}
 }
